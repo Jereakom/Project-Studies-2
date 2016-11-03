c54783feb12215c321879ad6aab64273c2b9b96f
damsu <damorizur@gmail.com>
2016-11-03 18:09:59 +0200

    Added registration fucntionnality with dummy page and working code, including validators.

--- connect.js
+++ connect.js
@@ -3,6 +3,7 @@ var username = null;
 var loginReturnJSON = [];
 var logindiv = document.getElementById('loginbutton');
 var logoutdiv = document.getElementById('logoutbutton');
+var registerdiv = document.getElementById('registerbutton');
 togglelogin();
 
 function login(name,pass){
@@ -46,11 +47,13 @@ togglelogin();
 function togglelogin (){
 	if (localStorage.getItem('username') == null) {
 		logindiv.style.display = 'block';
+		registerdiv.style.display = 'block';
 logoutdiv.style.display = 'none';
 loginline.style.display = 'none';
 	}
 	else {
 		logindiv.style.display = 'none';
+		registerdiv.style.display = 'none';
 logoutdiv.style.display = 'block';
 loginline.style.display = 'initial';
 
@@ -59,4 +62,38 @@ loginline.style.display = 'initial';
 
 function gotologin () {
 	window.location.href = "index.html";
+}
+
+function register (name, pass, conf_pass) {
+	var registerRequestArray = {"username":name,"password":pass};
+		var registerRequestJSON = JSON.stringify(registerRequestArray);
+		if (pass == conf_pass){
+	console.log(registerRequestJSON);
+	regRequest = new XMLHttpRequest();
+	var url = "http://project-studies-2.herokuapp.com/register";
+	regRequest.open("POST", url, false);
+	regRequest.setRequestHeader("Content-type", "application/json");
+	regRequest.onreadystatechange = function () { 
+	console.log(regRequest.readyState);
+	console.log(regRequest.status);
+    if (regRequest.readyState == 4 && regRequest.status == 200) {
+        if (regRequest.responseText == "User created successfully!") {
+		alert("Registration successful!\nPress OK to automatically login.");
+		console.log("beer");
+		login(name,pass);}
+		else if (regRequest.responseText == "User already exists!"){
+			alert("User already exist ! Please try a different Username");
+		}
+    } else {
+		alert("Registration failed, sorry!")
+	}
+	}
+regRequest.send(registerRequestJSON);
+		}
+		else {alert("Password confirmation is different.\nPlease try again!");}
+
+}
+
+function gotoregister () {
+	window.location.href = "register.html";
 }
