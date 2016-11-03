427830833e73ffeceb8cac35546f6deb3d43accc
Jaska <vaisanenjaska@gmail.com>
2016-11-03 09:48:26 +0200

    Put the board array into a separate file, and added a new game button.

--- othello.js
+++ othello.js
@@ -1,75 +1,4 @@
 
-
-var boardString = '000000000000000000000000000120000002100000000000000000000000000000000000';
-
-var boardArray = '{' +
-  '"0" : {"color" : "clear", "x" : 0, "y" : 0},' +
-  '"1" : {"color" : "clear", "x" : 1, "y" : 0},' +
-  '"2" : {"color" : "clear", "x" : 2, "y" : 0},' +
-  '"3" : {"color" : "clear", "x" : 3, "y" : 0},' +
-  '"4" : {"color" : "clear", "x" : 4, "y" : 0},' +
-  '"5" : {"color" : "clear", "x" : 5, "y" : 0},' +
-  '"6" : {"color" : "clear", "x" : 6, "y" : 0},' +
-  '"7" : {"color" : "clear", "x" : 7, "y" : 0},' +
-  '"8" : {"color" : "clear", "x" : 0, "y" : 1},' +
-  '"9" : {"color" : "clear", "x" : 1, "y" : 1},' +
-  '"10" : {"color" : "clear", "x" : 2, "y" : 1},' +
-  '"11" : {"color" : "clear", "x" : 3, "y" : 1},' +
-  '"12" : {"color" : "clear", "x" : 4, "y" : 1},' +
-  '"13" : {"color" : "clear", "x" : 5, "y" : 1},' +
-  '"14" : {"color" : "clear", "x" : 6, "y" : 1},' +
-  '"15" : {"color" : "clear", "x" : 7, "y" : 1},' +
-  '"16" : {"color" : "clear", "x" : 0, "y" : 2},' +
-  '"17" : {"color" : "clear", "x" : 1, "y" : 2},' +
-  '"18" : {"color" : "clear", "x" : 2, "y" : 2},' +
-  '"19" : {"color" : "clear", "x" : 3, "y" : 2},' +
-  '"20" : {"color" : "clear", "x" : 4, "y" : 2},' +
-  '"21" : {"color" : "clear", "x" : 5, "y" : 2},' +
-  '"22" : {"color" : "clear", "x" : 6, "y" : 2},' +
-  '"23" : {"color" : "clear", "x" : 7, "y" : 2},' +
-  '"24" : {"color" : "clear", "x" : 0, "y" : 3},' +
-  '"25" : {"color" : "clear", "x" : 1, "y" : 3},' +
-  '"26" : {"color" : "clear", "x" : 2, "y" : 3},' +
-  '"27" : {"color" : "white", "x" : 3, "y" : 3},' +
-  '"28" : {"color" : "black", "x" : 4, "y" : 3},' +
-  '"29" : {"color" : "clear", "x" : 5, "y" : 3},' +
-  '"30" : {"color" : "clear", "x" : 6, "y" : 3},' +
-  '"31" : {"color" : "clear", "x" : 7, "y" : 3},' +
-  '"32" : {"color" : "clear", "x" : 0, "y" : 4},' +
-  '"33" : {"color" : "clear", "x" : 1, "y" : 4},' +
-  '"34" : {"color" : "clear", "x" : 2, "y" : 4},' +
-  '"35" : {"color" : "black", "x" : 3, "y" : 4},' +
-  '"36" : {"color" : "white", "x" : 4, "y" : 4},' +
-  '"37" : {"color" : "clear", "x" : 5, "y" : 4},' +
-  '"38" : {"color" : "clear", "x" : 6, "y" : 4},' +
-  '"39" : {"color" : "clear", "x" : 7, "y" : 4},' +
-  '"40" : {"color" : "clear", "x" : 0, "y" : 5},' +
-  '"42" : {"color" : "clear", "x" : 2, "y" : 5},' +
-  '"41" : {"color" : "clear", "x" : 1, "y" : 5},' +
-  '"43" : {"color" : "clear", "x" : 3, "y" : 5},' +
-  '"44" : {"color" : "clear", "x" : 4, "y" : 5},' +
-  '"45" : {"color" : "clear", "x" : 5, "y" : 5},' +
-  '"46" : {"color" : "clear", "x" : 6, "y" : 5},' +
-  '"47" : {"color" : "clear", "x" : 7, "y" : 5},' +
-  '"48" : {"color" : "clear", "x" : 0, "y" : 6},' +
-  '"49" : {"color" : "clear", "x" : 1, "y" : 6},' +
-  '"50" : {"color" : "clear", "x" : 2, "y" : 6},' +
-  '"52" : {"color" : "clear", "x" : 4, "y" : 6},' +
-  '"51" : {"color" : "clear", "x" : 3, "y" : 6},' +
-  '"53" : {"color" : "clear", "x" : 5, "y" : 6},' +
-  '"54" : {"color" : "clear", "x" : 6, "y" : 6},' +
-  '"55" : {"color" : "clear", "x" : 7, "y" : 6},' +
-  '"56" : {"color" : "clear", "x" : 0, "y" : 7},' +
-  '"57" : {"color" : "clear", "x" : 1, "y" : 7},' +
-  '"58" : {"color" : "clear", "x" : 2, "y" : 7},' +
-  '"59" : {"color" : "clear", "x" : 3, "y" : 7},' +
-  '"60" : {"color" : "clear", "x" : 4, "y" : 7},' +
-  '"61" : {"color" : "clear", "x" : 5, "y" : 7},' +
-  '"62" : {"color" : "clear", "x" : 6, "y" : 7},' +
-  '"63" : {"color" : "clear", "x" : 7, "y" : 7}' +
-  '}'
-
-
   var bw = 400;
   var bh = 400;
   var p = 0;
@@ -233,5 +162,10 @@ var boardArray = '{' +
         board[i]["color"] = "black";
       }
     }
+    drawBoard();
+  }
+
+  function newGame() {
+    board = JSON.parse(boardArray);
     drawBoard();
   }
