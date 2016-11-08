0049b2b5f2e9b17cd556396d0ed97ee82bb0022d
damsu <damorizur@gmail.com>
2016-11-08 19:00:30 +0200

    removed useless console.logs

--- brain.js
+++ brain.js
@@ -1,23 +1,14 @@
 function randomAI() {
     var viable_moves = [];
-    console.log(turn);
-    console.log(!((turn % 2) != 0));
     if (!((turn % 2) != 0)) {
         for (var i = 0; i < 64; i++) {
-            console.log(board[i]["color"]);
             if (board[i]["color"] == "viable") {
                 viable_moves.push(i);
-                console.log(viable_moves);
             }
         }
         if (viable_moves.length > 0) {
             var random_move_position = viable_moves[Math.floor(Math.random() * (viable_moves.length - 1))];
-            console.log((turn % 2) != 0);
-
-            console.log(random_move_position);
-
             board[random_move_position]["color"] = "white";
-
         } else {
             console.log("no viable moves");
         }
